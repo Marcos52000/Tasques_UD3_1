c91d2db49ac8ba384b29977e086e4f1401df4698
Marcos52000 <55589715+Marcos52000@users.noreply.github.com>
2022-01-11 10:13:12 +0100

    Update

---
+++ Java01/src/UD2/Test4.java
@@ -0,0 +1,18 @@
+package UD2;
+import java.util.Iterator;
+
+public class Test4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola");
+		System.out.println("Adios");
+		System.out.println("Cucu");
+		for (int i = 0; i <6; i++) {
+			System.out.println(i+" ");
+			System.out.println("FIN DE PROGRAMA");
+			
+		}
+	}
+
+}
